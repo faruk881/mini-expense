a78b02d836e971bed50300c8fc162ce05c27bd39
MD OMAR FARUK <omarfaruk.likhon@gmail.com>
2026-01-10 14:32:58 +0600

    create expenses, expense category, user migration and database.

---
+++ app/Models/Expense.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'amount',
+        'description',
+        'image_url',
+        'status',
+        'remarks',
+    ];
+}
